410e809405deda2643588a76f00de81741abacbb
jb-sans <68002303+jb-sans@users.noreply.github.com>
2020-07-08 16:23:06 +0200

    Ajout de checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Transform playerSpawn;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerSpawn.position = transform.position;
+            //Pour ne pas avoir les checkpoints qui restent quand ils sont passés
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
