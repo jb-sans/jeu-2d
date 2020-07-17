a9ea45bdfe2951dda22df8a8c5d158c7e88c3b67
jb-sans <68002303+jb-sans@users.noreply.github.com>
2020-07-17 12:22:13 +0200

    Ajout de niveaux infinis
    Dans le niveau 3 avec la prefab

---
+++ Assets/TileManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public Grid grid;
+
+    private Transform playerTransform;
+    private float spawnX = 0.0f;
+    private float tileLength = 22.0f;
+    private int tilesOnScreen = 2;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < tilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(playerTransform.position.x > (spawnX - tilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(grid.transform);
+        go.transform.position = Vector2.right * spawnX;
+        spawnX += tileLength;
+    }
+
+}
